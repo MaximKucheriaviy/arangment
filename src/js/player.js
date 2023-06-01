c8122e4a7f32bc31b3da7708c2d98e8a67c227b9
MaximKucheriaviy <96634366+MaximKucheriaviy@users.noreply.github.com>
2023-06-01 16:36:49 +0300

    player logick created

--- src/js/player.js
+++ src/js/player.js
@@ -11,19 +11,15 @@ class Galaplayer {
     );
     this.createSlider();
     this.createButton();
+    this.sliderSetup();
     // завантажуємо звук
     this.song = new Audio("./Summerfogle.mp3");
     // мазначаємо методи для кнопок
-    this.startButton.addEventListener("click", () => {
-      this.song.play();
-    });
-    this.pauseButton.addEventListener("click", () => {
-      this.song.pause();
-    });
-    this.stopButton.addEventListener("click", () => {
-      this.song.pause();
-      this.song.currentTime = 0;
-    });
+    this.startButton.addEventListener("click", this.onPlay);
+    this.pauseButton.addEventListener("click", this.onPause);
+    this.stopButton.addEventListener("click", this.onStop);
+
+    this.moving = false;
   }
   createButton() {
     // створення нижньої панелі для кнопок
@@ -45,25 +41,89 @@ class Galaplayer {
   createSlider() {
     this.slider = document.createElement("div");
     this.handle = document.createElement("div");
+    this.xPos = 0;
 
     this.slider.setAttribute(
       "style",
       `height: 50%;
-      background-color: red;
+      background-color: bisque;
       display: flex;
-      align-items: center`
+      align-items: center;
+      padding-left: 10px;`
     );
 
     this.handle.setAttribute(
       "style",
-      `height: 20px;
+      `left: 0px;
+    height: 20px;
       width: 20px;
-     background-color: yellow`
+     background-color: cornflowerblue;
+     position: relative;`
     );
 
     this.slider.append(this.handle);
     this.rootDiv.append(this.slider);
   }
+  sliderSetup = () => {
+    this.rightMax = this.slider.offsetWidth - 40;
+    this.handle.addEventListener("mousedown", this.onMouseDown);
+  };
+  onMove = (event) => {
+    this.xPos += event.movementX;
+    if (this.xPos > this.rightMax) {
+      this.xPos = this.rightMax;
+    }
+    if (this.xPos < 0) {
+      this.xPos = 0;
+    }
+    this.setHandlePosition(this.xPos);
+  };
+  onMouseDown = () => {
+    document.addEventListener("mousemove", this.onMove);
+    document.addEventListener("mouseup", this.onMouseUp);
+    this.moving = true;
+  };
+  onMouseUp = () => {
+    document.removeEventListener("mousemove", this.onMove);
+    const present = (this.xPos * 100) / this.rightMax;
+    this.song.currentTime = (present * this.song.duration) / 100;
+    this.moving = false;
+  };
+  onPlay = () => {
+    this.song.play();
+    this.sliderInterval = setInterval(this.sliderProcess, 200);
+  };
+  onPause = () => {
+    clearInterval(this.sliderInterval);
+    this.song.pause();
+  };
+  onStop = () => {
+    clearInterval(this.sliderInterval);
+    this.song.pause();
+    this.song.currentTime = 0;
+    this.xPos = 0;
+    this.setHandlePosition();
+  };
+  sliderProcess = () => {
+    if (this.moving) {
+      return;
+    }
+    const time = this.song.currentTime;
+    const duration = this.song.duration;
+    const songPresent = (time * 100) / duration;
+    this.xPos = (songPresent * this.rightMax) / 100;
+    this.setHandlePosition();
+  };
+  setHandlePosition = () => {
+    this.handle.setAttribute(
+      "style",
+      `left: ${this.xPos}px;
+    height: 20px;
+      width: 20px;
+     background-color: cornflowerblue;
+     position: relative;`
+    );
+  };
 }
 
 const player = new Galaplayer({ path: "Hello", rootClassName: "audio" });
