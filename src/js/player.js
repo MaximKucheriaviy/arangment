6e51e7c02724c4050cf084d99900c173c6d52ee5
Maxim Kucheriaviy <maxim-k@i.ua>
2023-06-01 11:12:25 +0300

    player slider displaying

--- src/js/player.js
+++ src/js/player.js
@@ -2,17 +2,15 @@ class Galaplayer {
   constructor({ path, rootClassName }) {
     this.path = path;
     this.rootDiv = document.querySelector(`.${rootClassName}`);
-    // створення кнопок
-    this.startButton = document.createElement("button");
-    this.stopButton = document.createElement("button");
-    this.pauseButton = document.createElement("button");
-    this.startButton.textContent = "Start";
-    this.stopButton.textContent = "Stop";
-    this.pauseButton.textContent = "Pause";
-    // додавання кнопок в панель
-    this.rootDiv.append(this.startButton);
-    this.rootDiv.append(this.stopButton);
-    this.rootDiv.append(this.pauseButton);
+    this.rootDiv.setAttribute(
+      "style",
+      `
+      display: flex;
+      flex-direction: column;
+    `
+    );
+    this.createSlider();
+    this.createButton();
     // завантажуємо звук
     this.song = new Audio("./Summerfogle.mp3");
     // мазначаємо методи для кнопок
@@ -26,6 +24,45 @@ class Galaplayer {
       this.song.pause();
       this.song.currentTime = 0;
     });
+  }
+  createButton() {
+    // створення нижньої панелі для кнопок
+    const buttonDiv = document.createElement("div");
+    // створення кнопок
+    this.startButton = document.createElement("button");
+    this.stopButton = document.createElement("button");
+    this.pauseButton = document.createElement("button");
+    this.startButton.textContent = "Start";
+    this.stopButton.textContent = "Stop";
+    this.pauseButton.textContent = "Pause";
+    // додавання кнопок в панель
+    buttonDiv.append(this.startButton);
+    buttonDiv.append(this.stopButton);
+    buttonDiv.append(this.pauseButton);
+    //
+    this.rootDiv.append(buttonDiv);
+  }
+  createSlider() {
+    this.slider = document.createElement("div");
+    this.handle = document.createElement("div");
+
+    this.slider.setAttribute(
+      "style",
+      `height: 50%;
+      background-color: red;
+      display: flex;
+      align-items: center`
+    );
+
+    this.handle.setAttribute(
+      "style",
+      `height: 20px;
+      width: 20px;
+     background-color: yellow`
+    );
+
+    this.slider.append(this.handle);
+    this.rootDiv.append(this.slider);
   }
 }
 
