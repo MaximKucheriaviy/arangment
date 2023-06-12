ea9b618e1b9740ce032dc1273083d8caf4ac8ac3
MaximKucheriaviy <96634366+MaximKucheriaviy@users.noreply.github.com>
2023-06-12 19:32:20 +0300

    rot foto ready

--- src/js/photoSliderSrc.js
+++ src/js/photoSliderSrc.js
@@ -1,1 +1,71 @@
 const photosUrl = [];
+for (let i = 1; i <= 10; i++) {
+  photosUrl.push(`./RepetitionPhoto/photo${i}.jpg`);
+}
+class RotorHolder {
+  constructor(items) {
+    this.items = items;
+    this.current = 0;
+  }
+  getNext() {
+    this.current += 1;
+    if (this.current >= this.items.length) {
+      this.current = 0;
+    }
+    return this.getCurrent();
+  }
+  getCurrent() {
+    return this.items[this.current];
+  }
+  getPrevious() {
+    this.current -= 1;
+    if (this.current < 0) {
+      this.current = this.items.length - 1;
+    }
+    return this.getCurrent();
+  }
+}
+
+const addreses = new RotorHolder(photosUrl);
+let type = true;
+const nextButton = document.querySelector(".next-button");
+const prevButton = document.querySelector(".prev-button");
+
+const firstFoto = document.querySelector(".first-foto");
+const secondFoto = document.querySelector(".second-foto");
+
+function rightTurn() {
+  if (type) {
+    secondFoto.querySelector("img").setAttribute("src", addreses.getNext());
+  } else {
+    firstFoto.querySelector("img").setAttribute("src", addreses.getNext());
+  }
+  secondFoto.classList.toggle("hiddenFoto");
+  secondFoto.classList.toggle("visibleFoto");
+  firstFoto.classList.toggle("hiddenFoto");
+  firstFoto.classList.toggle("visibleFoto");
+  type = !type;
+}
+
+function leftTurn() {
+  if (type) {
+    secondFoto.querySelector("img").setAttribute("src", addreses.getPrevious());
+  } else {
+    firstFoto.querySelector("img").setAttribute("src", addreses.getPrevious());
+  }
+  secondFoto.classList.toggle("hiddenFoto");
+  secondFoto.classList.toggle("visibleFoto");
+  firstFoto.classList.toggle("hiddenFoto");
+  firstFoto.classList.toggle("visibleFoto");
+  type = !type;
+}
+
+nextButton.addEventListener("click", () => {
+  console.log(addreses.getCurrent());
+  rightTurn();
+});
+
+prevButton.addEventListener("click", () => {
+  console.log(addreses.getCurrent());
+  leftTurn();
+});
