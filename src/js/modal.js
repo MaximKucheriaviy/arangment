0a0295ed5efee969382987efd1e9bc428f4d6652
MaximKucheriaviy <96634366+MaximKucheriaviy@users.noreply.github.com>
2023-06-13 08:15:35 +0300

    backdrop ready

--- src/js/modal.js
+++ src/js/modal.js
@@ -1,1 +1,23 @@
+const modalFotos = document.querySelectorAll(".click-foto");
+const backdrop = document.querySelector(".backdrop");
+const modalImage = document.querySelector(".modal-image");
 
+modalFotos.forEach((item) => {
+  item.addEventListener("click", () => {
+    modalImage.setAttribute("src", item.getAttribute("src"));
+    setTimeout(() => {
+      backdrop.classList.toggle("visually-hidden");
+    }, 4);
+  });
+});
+
+backdrop.addEventListener("click", (event) => {
+  if (event.target !== backdrop) {
+    return;
+  }
+  backdrop.classList.add("transparent");
+  setTimeout(() => {
+    backdrop.classList.remove("transparent");
+    backdrop.classList.toggle("visually-hidden");
+  }, 500);
+});
